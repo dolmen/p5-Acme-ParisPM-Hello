2d0e61bcfabd34b46527fba7b9736f3dafacf65d
Olivier Mengué <dolmen@cpan.org>
2011-12-21 01:40:18 +0100

    I18N, options
    Internationalise.
    Ajoute des options sur la ligne de commande.

--- hello.pl
+++ hello.pl
@@ -2,5 +2,12 @@
 use 5.010;
 use strict;
 use warnings;
+use Term::Encoding;
+use Encode::Argv;
+use Getopt::Long;
 
-say "Hello, World!";
+my $lang = 'en';
+GetOptions("lang=s", \$lang) or die;
+my $who = $ARGV[0] // 'Paris.pm';
+say "Hello, $who!" if $lang eq 'en';
+say "Coucou, $who\N{U+A0}!" if $lang eq 'fr';
